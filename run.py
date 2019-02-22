88611251af55f265eaf9540c705d5172b432ad5e
Josh Thomas <personettelabs@gmail.com>
2019-02-21 19:23:34 -0500

    tests with protoss

--- run.py
+++ run.py
@@ -35,10 +35,10 @@ MAP_NAME = ""
 REALTIME = False
 
 # BUILD = sc2.AIBuild.RandomBuild
-BUILD = sc2.AIBuild.Rush
+# BUILD = sc2.AIBuild.Rush
 # BUILD = sc2.AIBuild.Timing
 # BUILD = sc2.AIBuild.Power
-# BUILD = sc2.AIBuild.Macro
+BUILD = sc2.AIBuild.Macro
 # BUILD = sc2.AIBuild.Air
 
 DIFFICULTY = sc2.Difficulty.CheatInsane
@@ -50,7 +50,7 @@ DIFFICULTY = sc2.Difficulty.CheatInsane
 # DIFFICULTY = sc2.Difficulty.Easy
 
 RACE = sc2.Race.Zerg
-ENEMY_RACE = sc2.Race.Zerg
+ENEMY_RACE = sc2.Race.Protoss
 REPLAY_NAME = os.path.join("replays", "last_lambdanaut_replay{}.*.sc2replay".format(datetime_str))
 
 if not MAP_NAME:
