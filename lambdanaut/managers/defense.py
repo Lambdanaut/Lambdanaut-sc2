a968e8124a057d6d20601d3d667f0f45676b6122
Josh Thomas <personettelabs@gmail.com>
2019-03-07 16:41:08 -0500

    better defense. defend near townhall rather than blindly chasing enemy

--- lambdanaut/managers/defense.py
+++ lambdanaut/managers/defense.py
@@ -54,107 +54,119 @@ class DefenseManager(StatefulManager):
 
     async def do_defending(self):
         worker_non_targets = {const.BANELING, const.REAPER}
+        townhall_distance_to_search = 45
         defending_worker_min_health = 0.3
 
-        for th in self.bot.townhalls:
+        townhalls = self.bot.townhalls
+
+        if townhalls:
+            # Get townhall with closest enemy unit by sorting them on their distance
+            townhalls_sorted = sorted([(self.bot.known_enemy_units.exclude_type(const2.NON_COMBATANTS).
+                                        closest_distance_to(th), th)
+                                       for th in townhalls])
+            closest_townhall = townhalls_sorted[0][1]
+
+            # Get nearby enemies
             enemies_nearby = [u.snapshot for u in self.bot.enemy_cache.values()
-                              if u.distance_to(th) < 45]
+                              if u.distance_to(closest_townhall) < townhall_distance_to_search]
 
-            if enemies_nearby:
-                # Publish message if there are multiple enemies
-                if not self.published_defending_against_multiple_enemies and \
-                        len(enemies_nearby) > 4:
-                    self.publish(Messages.DEFENDING_AGAINST_MULTIPLE_ENEMIES)
-                    self.published_defending_against_multiple_enemies = True
+            # Have army clusters defend
+            army_clusters = self.bot.army_clusters
 
-                # Workers attack enemy
-                ground_enemies = [enemy for enemy in enemies_nearby if not enemy.is_flying]
-                workers = self.bot.workers.closer_than(14, th.position.closest(enemies_nearby).position)
-                if ground_enemies and len(workers) > len(ground_enemies):
-                    for worker in workers:
-                        if worker.tag in self.bot.workers_defending:
-                            target = self.bot.closest_and_most_damaged(ground_enemies, worker)
-                            if target.type_id not in worker_non_targets:
-                                self.bot.actions.append(worker.attack(target))
+            # The harder we're attacked, the further-out army to pull back
+            # 1-3 Enemies: 0.3 of map. 4 enemy: 0.4 of map. 5 enemy: 0.5 of map. 6 enemy: 0.6 of map...
+            # 8 or more enemy: 0.8 of map
+            distance_ratio_to_pull_back = max(0.3, min(0.8, len(enemies_nearby) * 0.1))
+            army_clusters = \
+                [cluster for cluster in army_clusters
+                 if closest_townhall.distance_to(cluster.position) <
+                 self.bot.start_location_to_enemy_start_location_distance * distance_ratio_to_pull_back]
 
-                        else:
-                            # Add workers to defending workers and attack nearby enemy
-                            # Use one more worker than there are enemies
-                            # OR
-                            # If the enemy is a single worker, just send one worker
-                            if len(ground_enemies) == 1 and ground_enemies[0].type_id in const2.WORKERS:
-                                defend_with_more_workers = len(self.bot.workers_defending) < 1
-                            else:
-                                defend_with_more_workers = len(self.bot.workers_defending) <= len(ground_enemies)
+            if army_clusters and self.bot.enemy_clusters:
 
-                            if defend_with_more_workers \
-                                    and worker.health_percentage > defending_worker_min_health + 0.05:
+                nearest_enemy_cluster = closest_townhall.position.closest(self.bot.enemy_clusters)
+                for army_cluster in army_clusters:
+                    if army_cluster:
+
+                        for unit in army_cluster:
+                            if unit.type_id not in const2.NON_COMBATANTS \
+                                    and not self.bot.unit_is_busy(unit) \
+                                    and unit.tag not in self.bot.townhall_queens.values():
+
+                                # If we're too far from our defending spot. Attack-move to defending spot.
+                                target = closest_townhall.position.towards_with_random_angle(
+                                    nearest_enemy_cluster.position, +7)
+
+                                if unit.distance_to(target) > 12:
+                                    self.bot.actions.append(unit.attack(target))
+                                else:
+                                    # If we're near our defending spot. Attack enemy unit
+                                    enemy_target = self.bot.closest_and_most_damaged(enemies_nearby, unit)
+
+                                    if enemy_target and enemy_target.distance_to(target) < 12:
+                                        self.bot.actions.append(unit.attack(enemy_target))
+
+            # Do defending for each townhall
+            for th in townhalls:
+                enemies_nearby = [u.snapshot for u in self.bot.enemy_cache.values()
+                                  if u.distance_to(th) < townhall_distance_to_search]
+
+                if enemies_nearby:
+                    # Publish message if there are multiple enemies
+                    if not self.published_defending_against_multiple_enemies and \
+                            len(enemies_nearby) > 4:
+                        self.publish(Messages.DEFENDING_AGAINST_MULTIPLE_ENEMIES)
+                        self.published_defending_against_multiple_enemies = True
+
+                    # Workers attack enemy
+                    ground_enemies = [enemy for enemy in enemies_nearby if not enemy.is_flying]
+                    workers = self.bot.workers.closer_than(14, th.position.closest(enemies_nearby).position)
+                    if ground_enemies and len(workers) > len(ground_enemies):
+                        for worker in workers:
+                            if worker.tag in self.bot.workers_defending:
                                 target = self.bot.closest_and_most_damaged(ground_enemies, worker)
                                 if target.type_id not in worker_non_targets:
-                                    self.bot.workers_defending.add(worker.tag)
-                                    self.bot.actions.append(worker.attack(target.position))
-                else:
-                    # If they have more than us, stop the worker from defending
-                    for worker in workers:
-                        if worker.tag in self.bot.workers_defending:
-                            self.bot.workers_defending.remove(worker.tag)
-                            self.bot.actions.append(worker.stop())
+                                    self.bot.actions.append(worker.attack(target))
 
-                # Have nearest queen defend
-                queen_tag = self.bot.townhall_queens.get(th.tag)
-                if queen_tag is not None:
-                    queen = self.bot.units.find_by_tag(queen_tag)
-                    if queen is not None and len(enemies_nearby) < 3:
-                        # Only send the closest queen if the enemy is only a couple units
-                        target = self.bot.closest_and_most_damaged(enemies_nearby, queen)
-
-                        if target and queen.distance_to(target) < 18 and queen.distance_to(th) < 18 \
-                                and not queen.weapon_cooldown:
-                            if target.distance_to(queen) < queen.ground_range:
-                                self.bot.actions.append(queen.attack(target))
                             else:
-                                self.bot.actions.append(queen.attack(target.position))
-                        else:
-                            self.bot.actions.append(queen.attack(th.position))
+                                # Add workers to defending workers and attack nearby enemy
+                                # Use one more worker than there are enemies
+                                # OR
+                                # If the enemy is a single worker, just send one worker
+                                if len(ground_enemies) == 1 and ground_enemies[0].type_id in const2.WORKERS:
+                                    defend_with_more_workers = len(self.bot.workers_defending) < 1
+                                else:
+                                    defend_with_more_workers = len(self.bot.workers_defending) <= len(ground_enemies)
 
-                # Have army clusters defend
-                army_clusters = self.bot.army_clusters
+                                if defend_with_more_workers \
+                                        and worker.health_percentage > defending_worker_min_health + 0.05:
+                                    target = self.bot.closest_and_most_damaged(ground_enemies, worker)
+                                    if target.type_id not in worker_non_targets:
+                                        self.bot.workers_defending.add(worker.tag)
+                                        self.bot.actions.append(worker.attack(target.position))
+                    else:
+                        # If they have more than us, stop the worker from defending
+                        for worker in workers:
+                            if worker.tag in self.bot.workers_defending:
+                                self.bot.workers_defending.remove(worker.tag)
+                                self.bot.actions.append(worker.stop())
 
-                # The harder we're attacked, the further-out army to pull back
-                # 1-3 Enemies: 0.3 of map. 4 enemy: 0.4 of map. 5 enemy: 0.5 of map. 6 enemy: 0.6 of map...
-                # 8 or more enemy: 0.8 of map
-                distance_ratio_to_pull_back = max(0.3, min(0.8, len(enemies_nearby) * 0.1))
-                army_clusters = \
-                    [cluster for cluster in army_clusters
-                     if th.distance_to(cluster.position) <
-                     self.bot.start_location_to_enemy_start_location_distance * distance_ratio_to_pull_back]
+                    # Have nearest queen defend
+                    queen_tag = self.bot.townhall_queens.get(th.tag)
+                    if queen_tag is not None:
+                        queen = self.bot.units.find_by_tag(queen_tag)
+                        if queen is not None and len(enemies_nearby) < 3:
+                            # Only send the closest queen if the enemy is only a couple units
+                            target = self.bot.closest_and_most_damaged(enemies_nearby, queen)
 
-                if army_clusters and self.bot.enemy_clusters:
-
-                    nearest_enemy_cluster = th.position.closest(self.bot.enemy_clusters)
-                    for army_cluster in army_clusters:
-                        if army_cluster:
-                            nearby_spine_crawlers = self.bot.units(const.SPINECRAWLER).ready.\
-                                closer_than(14, nearest_enemy_cluster.position)
-
-                            for unit in army_cluster:
-                                if unit.type_id not in const2.NON_COMBATANTS \
-                                        and unit.tag not in self.bot.townhall_queens.values():
-
-                                    # Prefer defending with spine crawlers if they're nearby
-                                    if nearby_spine_crawlers and len(nearest_enemy_cluster) > 5\
-                                            and unit.distance_to(nearby_spine_crawlers.center) > 8:
-
-                                        target = nearby_spine_crawlers.center
-
-                                        if target:
-                                            self.bot.actions.append(unit.attack(target))
-
-                                    else:
-                                        target = self.bot.closest_and_most_damaged(enemies_nearby, unit)
-
-                                        if target and not self.bot.unit_is_busy(unit):
-                                            self.bot.actions.append(unit.attack(target))
+                            if target and queen.distance_to(target) < 18 and queen.distance_to(th) < 18 \
+                                    and not queen.weapon_cooldown:
+                                if target.distance_to(queen) < queen.ground_range:
+                                    self.bot.actions.append(queen.attack(target))
+                                else:
+                                    self.bot.actions.append(queen.attack(target.position))
+                            else:
+                                self.bot.actions.append(queen.attack(th.position))
 
             # Bring back defending workers that have drifted too far from town halls
             workers_defending_to_remove = set()
--- lambdanaut/managers/intel.py
+++ lambdanaut/managers/intel.py
@@ -98,7 +98,7 @@ class IntelManager(Manager):
         """Checks the map to see if there are any visible units we should counter with roach/hydra"""
         if not self.has_scouted_enemy_counter_with_roaches:
             enemy_counter_with_roach_types = {
-                const.ROACH, const.ROACHWARREN, const.HYDRALISK, const.LAIR, const.HYDRALISKDEN,
+                const.ROACH, const.ROACHWARREN, const.HYDRALISK, const.HYDRALISKDEN, const.LAIR,
                 const.HELLIONTANK, const.PLANETARYFORTRESS,
             }
 
