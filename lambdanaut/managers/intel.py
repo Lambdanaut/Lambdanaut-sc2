cfde3b827dadadfc942395a018d1ee627394bec1
Josh Thomas <personettelabs@gmail.com>
2019-03-11 21:29:41 -0400

    more committed ling rush

--- lambdanaut/managers/intel.py
+++ lambdanaut/managers/intel.py
@@ -314,12 +314,12 @@ class IntelManager(Manager):
 
             # Enemy expansions excluding the starting location
             enemy_expansions = self.bot.known_enemy_units(const2.TOWNHALLS).further_than(
-                11, self.bot.enemy_start_location).filter(lambda th: th.is_ready or th.build_progress > 0.8)
+                11, self.bot.enemy_start_location).filter(lambda th: th.is_ready or th.build_progress > 0.9)
 
             if self.bot.enemy_race is sc2.Race.Terran:
                 greedy_enough = len(enemy_expansions) > 0
-            elif self.bot.enemy_race is sc2.Race.Zerg:
-                greedy_enough = len(enemy_expansions) > 1
+            # elif self.bot.enemy_race is sc2.Race.Zerg:
+            #     greedy_enough = len(enemy_expansions) > 1
             elif self.bot.enemy_race is sc2.Race.Protoss:
                 greedy_enough = len(enemy_expansions) > 0
 
